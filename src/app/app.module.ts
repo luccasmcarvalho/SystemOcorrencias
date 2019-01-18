7d3c866710c7fd860f078fc673439c611cd6eaa1
luccasmcarvalho <39125512+luccasmcarvalho@users.noreply.github.com>
2019-01-17 21:53:00 -0300

    tela inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SapoMapasAPIComponent } from './sapo-mapas-api/sapo-mapas-api.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SapoMapasAPIComponent
+    SapoMapasAPIComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
